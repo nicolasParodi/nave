5a8d1611dcf6b50dff43327bc1cb7ed469ce13b9
PC-07\PC01 <nachoagus@live.com.ar>
2016-10-18 11:35:20 -0300

    ñdlasd

---
+++ nave/Assets/script/move nave.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movenave : MonoBehaviour {
+
+    private Rigidbody2D myRigidbody;
+
+	// Use this for initialization
+	void Start () {
+
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        HandleMovement();
+
+	}
+
+    private void HandleMovement(){
+
+        myRigidbody.velocity = Vector2.left;//x=-1,y=0,z=0;
+
+    }
+}
